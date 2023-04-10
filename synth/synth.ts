54e9bfaf265c40ea032a0c00da641348cac1c2f7
not without text <111718539+not-without-text@users.noreply.github.com>
2023-04-10 13:19:28 -0400

    Update synth.ts

--- synth/synth.ts
+++ synth/synth.ts
@@ -1650,7 +1650,7 @@ export class Instrument {
 	}
 	
 	public static frequencyFromPitch(pitch: number): number {
-		return 440.0 * Math.pow(2.0, (pitch - 69.0) / 12.0);
+		return 440.0 * Math.pow(2.0, (pitch - 69.0) / 19.0);
 	}
 	
 	public addEnvelope(target: number, index: number, envelope: number): void {
